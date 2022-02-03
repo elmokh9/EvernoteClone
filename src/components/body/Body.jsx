9213b6de1dbe60d5230153083613c190e812195f
Eshan Singh Awal <singheshan29@gmail.com>
2022-02-03 20:29:48 +0530

    cleaningCode

--- src/components/body/Body.jsx
+++ src/components/body/Body.jsx
@@ -5,7 +5,7 @@ const Body = ()=>{
     return( 
         <div className="textBody">
             <div className="textTitle"><h1>Tame your work, organize your life</h1></div>
-            <div className="textLine"><h5>Remember everything and tackle any project with your notes, tsks and schedule all in one place.</h5></div>            
+            <div className="textLine"><h5>Remember everything and tackle any project with your notes, tasks and schedule all in one place.</h5></div>            
             <button className="btn">Sign up for free</button>
             <p>Already have an account? Log in</p>
         </div>
